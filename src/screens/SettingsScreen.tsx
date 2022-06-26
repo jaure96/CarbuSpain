21f1f34f09d7a97ccb62ef584ff73cb53b340def
jaure96 <x.jauregi96@gmail.com>
2022-06-26 22:54:58 +0200

    creating tab navigation

---
+++ src/screens/SettingsScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const SettingsScreen = () => {
+  return (
+    <View>
+      <Text>This is the Settings screen</Text>
+    </View>
+  )
+}
+
+export default SettingsScreen
