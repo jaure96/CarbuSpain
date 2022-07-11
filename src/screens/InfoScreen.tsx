92d8d53135279b42438cd5cb6390e4661dddf887
jaure96 <x.jauregi96@gmail.com>
2022-07-11 21:53:14 +0200

    minor

--- src/screens/InfoScreen.tsx
+++ src/screens/InfoScreen.tsx
@@ -135,7 +135,7 @@ const styles = StyleSheet.create({
   },
   mainContainer: {
     flex: 1,
-    marginTop: -30,
+    marginTop: -10,
   },
   scrollableContainer: {
     justifyContent: 'flex-start',
